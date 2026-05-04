7244ed18112ce9ed185cb954390bf9c2a7a6cba6
jekasemo4kin <jekasemo4kin@gmail.com>
2026-05-04 12:19:26 +0300

    init: project setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app-container">
+        <h1>PokeAPI Search</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
